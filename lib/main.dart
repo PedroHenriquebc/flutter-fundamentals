2a2bf272faea261078750ddd6a83e7bceda3ebb3
Pedro Henrique <pedrohbc96@gmail.com>
2023-10-14 18:27:34 -0300

    feat: Gera números aleatórios

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -24,11 +26,26 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var numeroGerado = 0;
+  int _gerarNumAleatorio() {
+    Random numAleatorio = Random();
+    return numAleatorio.nextInt(1000);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Meu app"),
+      ),
+      body: Center(child: Text(numeroGerado.toString())),
+      floatingActionButton: FloatingActionButton (
+        child: const Icon(Icons.add_box_outlined),
+        onPressed: () {
+          setState(() {
+            numeroGerado = _gerarNumAleatorio();
+          });
+        }
       ),
     );
   }
