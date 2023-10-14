9eedc0b63c57a29c79662ac3235ea310c0916b75
Pedro Henrique <pedrohbc96@gmail.com>
2023-10-14 17:41:10 -0300

    feat: Adiciona widgets iniciais stateless e stateful

---
+++ android/app/src/main/kotlin/com/pedrohbc/trilhaapp/trilhaapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.pedrohbc.trilhaapp.trilhaapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.amber),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Meu app"),
+      ),
+    );
+  }
+}
